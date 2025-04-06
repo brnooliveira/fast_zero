e62b723a777f9094b42e942268e6e20570c0c8a7
brnooliveira <breno.osales123@gmail.com>
2025-04-06 19:41:52 -0300

    Add FastAPI app, root endpoint, and user creation endpoint

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,38 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+from .schemas import Message, UserDB, UserPublic, UserSchema
 
 app = FastAPI()
 
 
-@app.get('/')
+database = []
+
+
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá mundo!'}
+
+
+@app.get('/exercicio-html', response_class=HTMLResponse)
+def exercicio_aula_02():
+    return """
+    <html>
+      <head>
+        <title>Nosso olá mundo!</title>
+      </head>
+      <body>
+        <h1> Olá Mundo </h1>
+      </body>
+    </html>"""
+
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
+
+    database.append(user_with_id)
+
+    return user_with_id
--- tests/test_app.py
+++ tests/test_app.py
@@ -6,9 +6,38 @@ from fast_zero.app import app
 
 
 def test_root_deve_retornar_ok_e_ola_mundo():
-    client = TestClient(app) # Arrange
+    client = TestClient(app)  # Arrange
 
-    response = client.get('/') # Act
+    response = client.get('/')  # Act
 
-    assert response.status_code == HTTPStatus.OK # Assert
-    assert response.json() == {'message': 'Olá mundo!'} # Assert
+    assert response.status_code == HTTPStatus.OK  # Assert
+    assert response.json() == {'message': 'Olá mundo!'}  # Assert
+
+
+def test_exercicio_ola_mundo_em_html():
+    client = TestClient(app)
+
+    response = client.get('/exercicio-html')
+
+    assert response.status_code == HTTPStatus.OK
+    assert '<h1> Olá Mundo </h1>' in response.text
+
+
+def test_create_user():
+    client = TestClient(app)
+
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'Alice',
+            'email': 'alice@example.com',
+            'password': 'secret',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'username': 'Alice',
+        'email': 'alice@example.com',
+        'id': 1,
+    }
