1af63bb87d8b761c6c2e5b743e382cee888f491e
brnooliveira <breno.osales123@gmail.com>
2025-04-06 19:58:18 -0300

    Refactor user endpoints and add user list and update functionality

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,31 +1,21 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fast_zero.app import app
-
-
-def test_root_deve_retornar_ok_e_ola_mundo():
-    client = TestClient(app)  # Arrange
-
+def test_root_deve_retornar_ok_e_ola_mundo(client):
     response = client.get('/')  # Act
 
     assert response.status_code == HTTPStatus.OK  # Assert
     assert response.json() == {'message': 'Olá mundo!'}  # Assert
 
 
-def test_exercicio_ola_mundo_em_html():
-    client = TestClient(app)
-
+def test_exercicio_ola_mundo_em_html(client):
     response = client.get('/exercicio-html')
 
     assert response.status_code == HTTPStatus.OK
     assert '<h1> Olá Mundo </h1>' in response.text
 
 
-def test_create_user():
-    client = TestClient(app)
-
+def test_create_user(client):
     response = client.post(
         '/users/',
         json={
@@ -41,3 +31,49 @@ def test_create_user():
         'email': 'alice@example.com',
         'id': 1,
     }
+
+
+def test_read_users(client):
+    response = client.get('/users/')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'users': [
+            {
+                'username': 'Alice',
+                'email': 'alice@example.com',
+                'id': 1,
+            }
+        ]
+    }
+
+
+def test_update_user(client):
+    response = client.put(
+        '/users/1',
+        json={
+            'username': 'Bob',
+            'email': 'bob@example.com',
+            'password': 'newpassword',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'id': 1,
+        'username': 'Bob',
+        'email': 'bob@example.com',
+    }
+
+
+def test_update_user_caindo_na_exception(client):
+    response = client.put(
+        '/users/123123213',
+        json={
+            'username': 'Bob',
+            'email': 'bob@example.com',
+            'password': 'newpassword',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
